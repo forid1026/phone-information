573236ad56fe122cd4ee020916e837dfa6ab201b
forid1026 <sheikhforid1026@gmai.com>
2022-08-10 19:34:29 +0600

    all error handle done

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ const searchPhone = () => {
     document.getElementById("phone-wrapper").textContent = "";
     document.getElementById("phone-detail").textContent = "";
   } else {
-    document.getElementById("error-display").textContent = "";
     inputText.value = "";
+    document.getElementById("error-display").textContent = "";
     document.getElementById("phone-wrapper").textContent = "";
     document.getElementById("phone-detail").textContent = "";
     searchPhoneResult(searchText);
@@ -29,12 +29,21 @@ const searchPhoneResult = async (searchText) => {
 };
 
 const displayPhoneResult = (phone) => {
-  const phoneWrapper = document.getElementById("phone-wrapper");
-  phone.map((phoneInfo) => {
-    console.log("all search phone", phoneInfo);
-    const singlePhoneDiv = document.createElement("div");
-    singlePhoneDiv.classList.add("col-md-4", "mb-5");
-    singlePhoneDiv.innerHTML = `
+  console.log("phone", phone);
+  if (phone.length == 0) {
+    const errorDisplay = document.getElementById("error-display");
+    errorDisplay.innerHTML = `<h3 class="text-center text-danger">Your Search Result Not Found! Try Again</h3>`;
+    document.getElementById("phone-wrapper").textContent = "";
+    document.getElementById("phone-detail").textContent = "";
+  } else {
+    const phoneWrapper = document.getElementById("phone-wrapper");
+    console.log("total phone", phone.slice(0, 20));
+    phone
+    phone.slice(0, 20).map((phoneInfo) => {
+      console.log("all search phone", phoneInfo); 
+      const singlePhoneDiv = document.createElement("div");
+      singlePhoneDiv.classList.add("col-md-4", "mb-5");
+      singlePhoneDiv.innerHTML = `
       <div class="card">
           <img src="${phoneInfo.image}" class="card-img-top img-fluid img-thumbnail" alt="...">
           <div class="card-body">
@@ -44,8 +53,11 @@ const displayPhoneResult = (phone) => {
           </div>
       </div>
     `;
-    phoneWrapper.appendChild(singlePhoneDiv);
-  });
+      phoneWrapper.appendChild(singlePhoneDiv);
+    });
+
+    
+  }
 };
 
 // phone details data load
@@ -64,6 +76,29 @@ const displayPhoneDetails = (phoneDetail) => {
   const phoneDetailDiv = document.getElementById("phone-detail");
   const singleDetailDiv = document.createElement("div");
   singleDetailDiv.classList.add("col-md-6", "mb-5", "mx-auto", "d-block");
+  let others;
+  if (phoneDetail.others == null) {
+    others = "No other  features found";
+  } else {
+    others = `
+    <ul>
+    <li>Bluetooth: ${phoneDetail.others.Bluetooth}</li>
+    <li>GPS: ${phoneDetail.others.GPS}</li>
+    <li>NFC: ${phoneDetail.others.NFC}</li>
+    <li>Radio: ${phoneDetail.others.Radio}</li>
+    <li>USB: ${phoneDetail.others.USB}</li>
+    <li>WLAN: ${phoneDetail.others.WLAN}</li>
+  </ul> 
+    
+    `;
+  }
+  let releaseDate;
+  if (phoneDetail.releaseDate == "") {
+    releaseDate = "No Release Date Found";
+  } else {
+    releaseDate = phoneDetail.releaseDate;
+  }
+
   singleDetailDiv.innerHTML = `
   
        <div class="card">
@@ -76,20 +111,23 @@ const displayPhoneDetails = (phoneDetail) => {
               <li>Chip Set: ${phoneDetail.mainFeatures.chipSet}</li>
               <li>Display Size: ${phoneDetail.mainFeatures.displaySize}</li>
               <li>Memory: ${phoneDetail.mainFeatures.memory}</li>
+              <li>Sensor:
+                ${phoneDetail.mainFeatures.sensors[0]},
+                 ${phoneDetail.mainFeatures.sensors[1]},
+                 ${phoneDetail.mainFeatures.sensors[2]} ,
+                 ${phoneDetail.mainFeatures.sensors[3]} 
+                 ${phoneDetail.mainFeatures.sensors[4]}, 
+                 ${phoneDetail.mainFeatures.sensors[5]} 
+              </li>
             </ul>
-            <p class="card-text">Others Features : </p>
-            <ul>
-              <li>Bluetooth: ${phoneDetail.others.Bluetooth}</li>
-              <li>GPS: ${phoneDetail.others.GPS}</li>
-              <li>NFC: ${phoneDetail.others.NFC}</li>
-              <li>Radio: ${phoneDetail.others.Radio}</li>
-              <li>USB: ${phoneDetail.others.USB}</li>
-              <li>WLAN: ${phoneDetail.others.USB}</li>
-              <li>${phoneDetail.others.WLAN}</li>
-            </ul>  
-            <p class="card-text">Release Date : ${phoneDetail.releaseDate}</p>
+            <p class="card-text">Others Features : ${others}</p>
+             
+            <p class="card-text">Release Date : ${releaseDate}</p>
            </div>
       </div>
   `;
+
+  document.getElementById("phone-detail").textContent = "";
   phoneDetailDiv.appendChild(singleDetailDiv);
 };
+
